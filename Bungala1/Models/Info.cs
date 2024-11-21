3f852c836fc63fc8c0525369009e0a129bb36c5b
LORD <LORD@DESKTOP-LLUGA33>
2024-11-21 22:45:56 +0200

    Add project files.

---
+++ Bungala1/Models/Info.cs
@@ -0,0 +1,10 @@
+﻿namespace Bungala1.Models
+{
+    public class Info
+    {
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public int Phone_Number { get; set; }
+        public string List {get; set;}
+    }
+}
---
+++ Bungala1/Models/ShrekMitiu.cs
@@ -0,0 +1,12 @@
+﻿namespace Bungala1.Models
+{
+    public class ShrekMitiu
+    {
+        public int Id { get; set; }
+        public int Seats { get; set; }
+        public bool IsSmokerFriendly { get; set; }
+        public bool IsPetFriendly { get; set; }
+        public string Location { get; set; }
+        public decimal PricePerNight { get; set; }
+    }
+}
